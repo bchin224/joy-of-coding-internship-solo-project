402e867dea81abff6779585bd901fe1b132ca7ae
bchin224 <bchin224@gmail.com>
2024-03-08 22:23:04 -0500

    nav bar

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Link from 'next/link'
+import React from 'react'
+import { MdOutlineEventNote } from "react-icons/md";
+
+
+const NavBar = () => {
+    const links = [
+        { label: 'Today', href: '/today' },
+        { label: 'This Week', href: '/week' }
+    ]
+
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+        <Link href='/'><MdOutlineEventNote/></Link>
+        {/* <Link href='/'>Logo</Link> */}
+        <ul className='flex space-x-6'>
+            {links.map(link =>
+                <Link
+                    key={link.href}
+                    className='text-zinc-200 hover:text-zinc-500 transition-colors: true'
+                    href={link.href}>{link.label}
+                </Link>
+            )}
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
