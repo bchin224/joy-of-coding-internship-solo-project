8c9267d29f91604a81fe135f2fdd47cf96dceaa4
bchin224 <bchin224@gmail.com>
2024-03-23 15:47:22 -0400

    To Do Patch Route

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-// import { createToDoSchema } from "../../validationSchema";
+import { createToDoSchema } from "../../../validationSchema";
 
 // Delete Todo
 export async function DELETE(
@@ -17,6 +17,38 @@ export async function DELETE(
         });
         return NextResponse.json(todo, { status: 201 })
     }
+}
 
+// Edit Todo
+export async function PATCH(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const body = await request.json();
+    const validation = createToDoSchema.safeParse(body)
 
+    console.log('Testing validation:', validation)
+
+    if (!validation.success) {
+        console.log('Fail:', validation)
+        return NextResponse.json(validation.error.format(), { status: 400 });
+    }
+
+    const todo = await prisma.toDo.findUniqueOrThrow({
+        where: { id: parseInt(params.id)}
+    })
+    
+    // make edit
+    const editTodo = await prisma.toDo.update({
+        where: { id: todo.id },
+        data: {
+            title: body.title,
+            description: body.description,
+            dueDate: body.dueDate,
+            status: body.status
+        }
+    })
+    return NextResponse.json(editTodo)
+
+    
 }
