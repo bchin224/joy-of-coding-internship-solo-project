6d7165047455b024b93787e4192f4aaf50f551b4
bchin224 <bchin224@gmail.com>
2024-03-26 00:25:45 -0400

    add edit schema validation

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -5,3 +5,10 @@ export const createToDoSchema = z.object({
     description: z.string().min(1, 'Description is required'),
     dueDate: z.string().min(1, 'Due Date is required')
 });
+
+export const editToDoSchema = z.object({
+    title: z.string().min(1, 'Title is required').max(255),
+    description: z.string().min(1, 'Description is required'),
+    dueDate: z.string().min(1, 'Due Date is required'),
+    status: z.string().min(1, 'Status is required')
+});
